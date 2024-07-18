6a79aff054d66eaeaaaef4dec0d4f96b52e89e13
parjwol <thapaparjwol2@gmail.com>
2024-07-18 09:34:36 +0545

    18th july

--- app.js
+++ app.js
@@ -10,7 +10,22 @@ lang.addEventListener("dblclick",()=>{
 i.addEventListener("click",()=>{
     document.querySelector(".dropdown2").style.display="block";
  
-})
+});
 i.addEventListener("dblclick",()=>{
     document.querySelector(".dropdown2").style.display="none";
-})
+});
+let scrollBar = document.querySelector(".gallery");
+let backBtn= document.getElementById("backbtn");
+let nextBtn = document.getElementById("nextbtn");
+scrollBar.addEventListener("wheel",(evt)=>{
+    evt.preventDefault();
+    scrollBar.scrollLeft += evt.deltaY;
+});
+nextBtn.addEventListener("click",()=>{
+    scrollBar.scrollLeft += 900;
+    console.log("clicked");
+});
+backBtn.addEventListener("click",()=>{
+    scrollBar.scrollLeft -= 900;
+    console.log("clicked");
+});
